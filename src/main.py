5e9484888f12a7996d791256052871064a0f2506
Sadia Islam Siddiqua <sadiasiddiqua16payel@gmail.com>
2026-05-02 00:02:44 +0600

    Add multiply function

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+print("Name: Payel")
+print("Today's Date:", datetime.now().date())
+
+from utils import add, subtract
+
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(10, 4))
+
